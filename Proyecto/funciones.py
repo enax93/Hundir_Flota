5f1b3e4422781bf27cb7a3f7ee050edd6894947c
mankolepanto <40011445+mankolepanto@users.noreply.github.com>
2024-03-02 14:32:50 +0100

    mi parte

--- Proyecto/funciones.py
+++ Proyecto/funciones.py
@@ -60,3 +60,52 @@ def recibir_ataque(tablero_jugador, fila, columna, coordenadas_atacadas):
         tablero_jugador[fila][columna] = 'O'  # Marcar como agua en el tablero del jugador
         coordenadas_atacadas.add((fila, columna))  # Agregar coordenadas a las atacadas
         return "Agua", True
+    
+
+def agregar_barco(tablero, tamano_barco): # esta es la variable principal
+    direccion = random.choice(['norte', 'sur', 'este', 'oeste'])
+    if direccion == 'norte':
+        fila_inicio = random.randint(tamano_barco - 1, len(tablero) - 1)
+        columna_inicio = random.randint(0, len(tablero[0]) - 1)
+        if fila_inicio - tamano_barco + 1 >= 0:
+            for i in range(fila_inicio, fila_inicio - tamano_barco, -1):
+                if tablero[i][columna_inicio] != " ":
+                    return False
+            for i in range(fila_inicio, fila_inicio - tamano_barco, -1):
+                tablero[i][columna_inicio] = "B"
+            return True
+        
+    elif direccion == 'sur':
+        fila_inicio = random.randint(0, len(tablero) - tamano_barco)
+        columna_inicio = random.randint(0, len(tablero[0]) - 1)
+        if fila_inicio + tamano_barco <= len(tablero):
+            for i in range(fila_inicio, fila_inicio + tamano_barco):
+                if tablero[i][columna_inicio] != " ":
+                    return False
+            for i in range(fila_inicio, fila_inicio + tamano_barco):
+                tablero[i][columna_inicio] = "B"
+            return True
+        
+    elif direccion == 'este':
+        fila_inicio = random.randint(0, len(tablero) - 1)
+        columna_inicio = random.randint(0, len(tablero[0]) - tamano_barco)
+        if columna_inicio + tamano_barco <= len(tablero[0]):
+            for i in range(columna_inicio, columna_inicio + tamano_barco):
+                if tablero[fila_inicio][i] != " ":
+                    return False
+            for i in range(columna_inicio, columna_inicio + tamano_barco):
+                tablero[fila_inicio][i] = "B"
+            return True
+        
+    elif direccion == 'oeste':
+        fila_inicio = random.randint(0, len(tablero) - 1)
+        columna_inicio = random.randint(tamano_barco - 1, len(tablero[0]) - 1)
+        if columna_inicio - tamano_barco + 1 >= 0:
+            for i in range(columna_inicio, columna_inicio - tamano_barco, -1):
+                if tablero[fila_inicio][i] != " ":
+                    return False
+            for i in range(columna_inicio, columna_inicio - tamano_barco, -1):
+                tablero[fila_inicio][i] = "B"
+            return True
+
+    
