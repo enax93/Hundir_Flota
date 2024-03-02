033d21a7d83193403ce7046f82b9d5c94392548b
ivanet8 <ivrodia@gmail.com>
2024-03-02 14:39:13 +0100

    clases

--- Proyecto/clases.py
+++ Proyecto/clases.py
@@ -4,7 +4,7 @@ from librerias import *
 class TableroFlota():
 
     # Función de inicialización. Entradas: jugador, barcos (nº y tamaño) y dimensión del tablero.
-    def __init__(self, jugador, barcos, dimension):
+    def __init__(self, jugador, barcos, dimension_tablero):
         self.jugador = jugador
-        self.dimension = dimension
+        self.dimension = dimension_tablero
         self.barcos = barcos
